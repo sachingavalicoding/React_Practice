682209c19db6c66d5a9ebd8ac206a0ea3f50cf52
Sachin Gavali <sachingavali321@gmail.com>
2023-06-18 22:27:41 +0530

    adding some features

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
-import { useState} from 'react';
+import './assets/w-1.png'
 function App() {
-  const [count , setCount] = useState(0);
-  const add = ()=>{
-    setCount(count + 1 );
-  }
-  const sub = ()=>{
-    setCount(count - 1 );
-  }
+  
   return (
-   <div className='main'>
-       <button onClick={add}
-        style={{
-          padding:'20px 30px',
-          color:'red',
-          backgroundColor:'black',
-          border:'none',
-          borderRadius:'10px',
-          fontSize:'50px'
-        }}
-        >  + </button>
-        <span>{count} </span>
-       <button onClick={sub}
-        style={{
-          padding:'20px 30px',
-          color:'red',
-          backgroundColor:'black',
-          border:'none',
-          borderRadius:'10px',
-          fontSize:'50px'
-        }}
-        >  - </button>
-   </div>
+   <main className='main'>
+      
+   </main>
   );
 }
 
